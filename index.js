a5892cc61eafa72e274045b90592b10dd91604ca
Diego <lima.j.diego@gmail.com>
2021-07-26 16:52:48 -0300

    hello bottr

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const Bottr = require('bottr')
 const BottrApp = require('bottr-app')
 const bot = new Bottr.Bot()
 
+bot.on('message_received', (message, session)=> {
+    session.send('Hello Word!')
+})
+
 bot.use(new BottrApp())
 bot.listen()
